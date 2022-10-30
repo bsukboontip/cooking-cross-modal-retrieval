55b2369781d7546e83b41f574f0ef0304d5633d6
Ben Sukboontip <ben.sukboontip@gmail.com>
2022-10-30 00:16:54 -0400

    added baseline model

---
+++ baseline_model/scripts/get_vocab.py
@@ -0,0 +1,16 @@
+import word2vec
+import sys
+import os
+
+'''
+Usage: python get_vocab.py /path/to/vocab.bin
+'''
+w2v_file = sys.argv[1]
+model = word2vec.load(w2v_file)
+
+vocab =  model.vocab
+
+# print("Writing to %s..." % os.path.join(os.path.dirname(w2v_file),'vocab.txt'))
+f = open(os.path.join(os.path.dirname(w2v_file),'vocab.txt'),'w')
+f.write("\n".join(vocab))
+f.close()
