56193dcc6cc758382af8b2bef9f6780aafe9c459
Adhokshaja Madhwaraj <amadhwar@andrew.cmu.edu>
2022-12-08 08:16:32 +0000

    final updates, working with occassional CUDA memory issue

--- sequential-autoencoder/train.py
+++ sequential-autoencoder/train.py
@@ -13,11 +13,12 @@ from dataset import RecipeDataset, collate
 from loss import TripletLoss, multi_label_loss
 import gc
 from evaluate import run_evaluation
+import wandb
+from tqdm import tqdm
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-
-def train_one_epoch(args, encoder, decoder, optimizer, train_loader, batch_triplet_loss, label_loss, epoch, scaler):
+def train_one_epoch(args, encoder, decoder, optimizer, train_loader, batch_triplet_loss, label_loss, epoch, scaler, count):
     
     # train
     encoder.train()
@@ -27,7 +28,11 @@ def train_one_epoch(args, encoder, decoder, optimizer, train_loader, batch_tripl
     epoch_batch_triplet_loss = 0.0
     epoch_label_loss = 0.0
 
-    for batch in train_loader:
+    for i, batch in tqdm(enumerate(train_loader), total=4000):
+        if i == 4000:
+            return epoch_batch_triplet_loss, epoch_label_loss, count
+        
+        count += 1
 
         # get the data
         with autocast(dtype=torch.float16):
@@ -35,8 +40,6 @@ def train_one_epoch(args, encoder, decoder, optimizer, train_loader, batch_tripl
             ingredient_embeddings = batch['ingredient_embeddings'].type(torch.float16).to(device)
             ingredient_indexes = batch['ingredient_indexes'].type(torch.float16).to(device)
             instruction_embeddings = batch['instruction_embeddings'].type(torch.float16).to(device)
-            
-            print(f"ingredients: {ingredient_embeddings.size()}, instructions: {instruction_embeddings.shape}")
 
             image_embeddings = batch['image_embeddings'].type(torch.float16).to(device)
             ingredient_lens = batch['ingredient_lens'].type(torch.int8)
@@ -67,7 +70,6 @@ def train_one_epoch(args, encoder, decoder, optimizer, train_loader, batch_tripl
 
             # Using this output, compute the average logits across ingredient sequences
             outputs = outputs / num_ingredients
-            print(f"New outputs shape: {outputs.shape}")
 
             label_loss = F.binary_cross_entropy_with_logits(outputs, ingredient_indexes)
             batch_loss = batch_loss / num_ingredients
@@ -77,14 +79,21 @@ def train_one_epoch(args, encoder, decoder, optimizer, train_loader, batch_tripl
             epoch_batch_triplet_loss += batch_loss.item()
             epoch_label_loss += label_loss.item()
 
-            print(batch_loss.item(), label_loss.item())
-
         # --------------------- BACKWARD PASS ---------------------
 
         optimizer.zero_grad()
         scaler.scale(loss).backward()
         scaler.step(optimizer)
         scaler.update()
+
+        # --------------------- WANDB LOGGING ----------------------
+
+        if count % args.wandb_log_every == 0:
+            wandb.log({
+                'batch_loss': batch_loss.item(),
+                'label_loss': label_loss.item(),
+            })
+
 
         # --------------------- GARBAGE COLLECTION ---------------------
         
@@ -96,11 +105,12 @@ def train_one_epoch(args, encoder, decoder, optimizer, train_loader, batch_tripl
         torch.cuda.reset_max_memory_cached(device=device)
 
 
-    return epoch_batch_triplet_loss, epoch_label_loss
+    return epoch_batch_triplet_loss, epoch_label_loss, count
 
 
 def train():
     args = get_args()
+    wandb.init(config=args, project="multimodal-sequential-autoencoder", entity="adhoki")
 
     # set the seed
     torch.manual_seed(args.seed)
@@ -112,11 +122,11 @@ def train():
 
     # load the data
     train_dataset = RecipeDataset(
-        partition='test',
-        ids_pkl='/home/ubuntu/recipe-dataset/test/test_keys.pkl', 
-        cleaned_layers='/home/ubuntu/recipe-dataset/json/cleaned_layers.json', 
+        partition='val',
+        ids_pkl='/home/ubuntu/recipe-dataset/val/val_keys.pkl', 
+        cleaned_layers='/home/ubuntu/recipe-dataset/json/cleaned_layers.json',
         image_map='/home/ubuntu/recipe-dataset/json/image_map.json', 
-        dataset_images='/home/ubuntu/recipe-dataset/test/', 
+        dataset_images='/home/ubuntu/recipe-dataset/val/', 
         bert_embeddings='/home/ubuntu/recipe-dataset/json/vocab_bert.pkl',
         ingredient_vocabulary='/home/ubuntu/recipe-dataset/json/ingredient_vocab.pkl',
         image_logs='/home/ubuntu/cooking-cross-modal-retrieval/sequential-autoencoder/logs',
@@ -137,11 +147,34 @@ def train():
         collate_fn=collate
     )
 
+    val_dataset = RecipeDataset(
+        partition='test',
+        ids_pkl='/home/ubuntu/recipe-dataset/test/test_keys.pkl', 
+        cleaned_layers='/home/ubuntu/recipe-dataset/json/cleaned_layers.json', 
+        image_map='/home/ubuntu/recipe-dataset/json/image_map.json', 
+        dataset_images='/home/ubuntu/recipe-dataset/test/', 
+        bert_embeddings='/home/ubuntu/recipe-dataset/json/vocab_bert.pkl',
+        ingredient_vocabulary='/home/ubuntu/recipe-dataset/json/ingredient_vocab.pkl',
+        image_logs='/home/ubuntu/cooking-cross-modal-retrieval/sequential-autoencoder/logs',
+        transform=transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+    )
+
+    val_loader = DataLoader(
+        val_dataset,
+        batch_size=args.batch_size,
+        # change this later
+        shuffle=True,
+        num_workers=args.num_workers,
+        collate_fn=lambda x: collate(x, need_metadata=True)
+    )
+
     # create the model
     encoder = CombinedEncoder()
-    # randomly saving the model
-    torch.save(encoder.state_dict(), 'checkpoints/best/encoder.pt')
-    print(f"SAVED THE ENCODER MODEL...")
     batch_triplet_loss = TripletLoss()
     hidden_dims = [512, 512]
     decoder = LinearDecoder(vocab_size=args.vocab_size, input_dim=args.hidden_dim*2, hidden_dims=hidden_dims)
@@ -149,20 +182,21 @@ def train():
     encoder = encoder.to(device)
     decoder = decoder.to(device)
 
-    print("MEMORY AFTER MODEL CREATION")
-    print("torch.cuda.memory_allocated: %fGB"%(torch.cuda.memory_allocated(0)/1024/1024/1024))
-    print("torch.cuda.memory_reserved: %fGB"%(torch.cuda.memory_reserved(0)/1024/1024/1024))
-    print("torch.cuda.max_memory_reserved: %fGB"%(torch.cuda.max_memory_reserved(0)/1024/1024/1024))
-    print("------------------------------------------------------------------------------------------------------------")
-
     # create the optimizer
     optimizer = torch.optim.Adam(list(encoder.parameters()) + list(decoder.parameters()), lr=args.lr, weight_decay=args.weight_decay)
+    count = 0
 
     for i in range(args.epochs):
-        epoch_batch_triplet_loss, epoch_label_loss = train_one_epoch(args, encoder, decoder, optimizer, train_loader, batch_triplet_loss, multi_label_loss, i, scaler)
+        epoch_batch_triplet_loss, epoch_label_loss, count = train_one_epoch(args, encoder, decoder, optimizer, train_loader, batch_triplet_loss, multi_label_loss, i, scaler, count)
 
         if args.evaluate_freq > 0 and i % args.evaluate_freq == 0:
-            run_evaluation('im2recipe', encoder, train_loader, args.medr_N, device)
+            medr, recall, closest_image_dict = run_evaluation('im2recipe', encoder, train_loader, args.medr_N, device)
+            wandb.log({
+                'epoch': i,
+                'medr': medr,
+                'recall': recall,
+            })
+            print(f"EVALUATION: EPOCH: {i}, MEDR: {medr}, RECALL: {recall}")
 
         if args.save_freq > 0 and i % args.save_freq == 0:
             torch.save({
@@ -171,7 +205,8 @@ def train():
                 'optimizer': optimizer.state_dict(),
                 'epoch': i,
                 'batch_triplet_loss': epoch_batch_triplet_loss,
-                'label_loss': epoch_label_loss
+                'label_loss': epoch_label_loss,
+                'args': args
             }, 'checkpoints/epoch_{}.pt'.format(i))
 
         if epoch_batch_triplet_loss + epoch_label_loss < best_loss:
@@ -182,8 +217,9 @@ def train():
                 'optimizer': optimizer.state_dict(),
                 'epoch': i,
                 'batch_triplet_loss': epoch_batch_triplet_loss,
-                'label_loss': epoch_label_loss
-            }, 'checkpoints/best/model.pt'.format(i))
+                'label_loss': epoch_label_loss,
+                'args': args
+            }, 'checkpoints/best/model.pt')
 
     return
     
--- sequential-autoencoder/utils/config.py
+++ sequential-autoencoder/utils/config.py
@@ -1,6 +1,5 @@
 import argparse
 
-
 def get_args():
 
     parser = argparse.ArgumentParser(description='PyTorch Recipe1M Training')
@@ -13,9 +12,10 @@ def get_args():
     parser.add_argument('--batch_size', default=4, type=int)
     parser.add_argument('--lr', default=0.0001, type=float)
     parser.add_argument('--weight_decay', default=0.0001, type=float)
-    parser.add_argument('--num_workers', default=1, type=int)
-    parser.add_argument('--evaluate_freq', default=10, type=int)
-    parser.add_argument('--save_freq', default=10, type=int)
+    parser.add_argument('--num_workers', default=4, type=int)
+    parser.add_argument('--evaluate_freq', default=1, type=int)
+    parser.add_argument('--save_freq', default=1, type=int)
+    parser.add_argument('--wandb_log_every', default=20, type=int)
     
     # TODO: Reverify this later, once things are loaded
     parser.add_argument('--vocab_size', default=9226, type=int)
@@ -30,12 +30,12 @@ def get_args():
     parser.add_argument('--ce_loss_weight', default=1.0, type=float)
     
     # Model saving and checkpointing options
-    parser.add_argument('--save_dir', default='checkpoints', type=str)
+    parser.add_argument('--save_dir', default='/checkpoints', type=str)
     parser.add_argument('--resume', default=False, type=bool)
     parser.add_argument('--resume_path', default=None, type=str)
 
     # Model evaluation options
-    parser.add_argument('--retrieval_mode', default='image2recipe', type=str, choices=['image2recipe', 'recipe2image'])
+    parser.add_argument('--retrieval_mode', default='im2recipe', type=str, choices=['im2recipe', 'recipe2im'])
     parser.add_argument('--medr_N', default=1000, type=int)
 
     args = parser.parse_args()
